06c7d693cfa4e6fe3fe201ed175e0f6f677d3712
Yaroslav Harmash <feroxes@gmail.com>
2022-01-21 09:37:37 +0200

    updated init cmd validation

--- uu_places_maing01-server/app/api/validation_types/parkingman-main-types.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable */
-const initDtoInType = shape({
-  name: uu5String(512).isRequired(),
-});
---
+++ uu_places_maing01-server/app/api/validation_types/places-main-types.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+const initDtoInType = shape({
+  name: uu5String(512).isRequired(),
+  reservationsConfig: shape({
+    dayOfStartReservations: number(1, 7).isRequired(),
+    hourOfStartReservations: number(1, 24).isRequired(),
+  }).isRequired(),
+});
